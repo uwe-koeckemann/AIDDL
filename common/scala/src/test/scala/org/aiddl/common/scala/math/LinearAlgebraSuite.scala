67851dc9da6fe404b1a91a9722260e0dafdde5a7
Uwe Köckemann <uweewu@gmail.com>
2022-09-14 17:17:58 +0200

    Minor fix

--- common/scala/src/test/scala/org/aiddl/common/scala/math/LinearAlgebraSuite.scala
+++ common/scala/src/test/scala/org/aiddl/common/scala/math/LinearAlgebraSuite.scala
@@ -10,8 +10,11 @@ import org.aiddl.core.scala.parser.Parser
 
 import org.aiddl.common.scala.math.linear_algebra._
 
-import org.aiddl.core.scala.representation.given_Conversion_Int_Num
-import org.aiddl.core.scala.representation.given_Conversion_Double_Num
+//import org.aiddl.core.scala.representation.given_Conversion_Int_Num
+//import org.aiddl.core.scala.representation.given_Conversion_Double_Num
+
+import org.aiddl.core.scala.representation.TermImplicits.int2Num
+import org.aiddl.core.scala.representation.TermImplicits.double2Num
 
 /**
  *  References:

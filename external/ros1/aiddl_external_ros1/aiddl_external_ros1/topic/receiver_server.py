f3912c4ebb390ba09bd3f70d5e7ca2d2b5bdaae5
Uwe Köckemann <uwe.kockemann@oru.se>
2023-01-24 13:30:37 +0100

    Fixing proto sources in aiddl_external_ros1 lib

--- external/ros1/aiddl_external_ros1/aiddl_external_ros1/topic/receiver_server.py
+++ external/ros1/aiddl_external_ros1/aiddl_external_ros1/topic/receiver_server.py
@@ -4,9 +4,9 @@ import grpc
 import rospy
 
 from aiddl_core.parser import parser
-import aiddl_external_ros1.proto.aiddl_msg_pb2 as aiddl_msg_pb2
-import aiddl_external_ros1.proto.receiver_pb2 as receiver_pb2
-import aiddl_external_ros1.proto.receiver_pb2_grpc as receiver_pb2_grpc
+import aiddl_external_grpc_python.generated.aiddl_msg_pb2 as aiddl_msg_pb2
+import aiddl_external_grpc_python.generated.receiver_pb2 as receiver_pb2
+import aiddl_external_grpc_python.generated.receiver_pb2_grpc as receiver_pb2_grpc
 
 from aiddl_external_grpc_python.receiver import ReceiverServer
 

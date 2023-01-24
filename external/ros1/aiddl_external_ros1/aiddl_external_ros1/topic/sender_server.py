c6e6652cafcd885240389454cd9c179c29f81808
Uwe Köckemann <uwe.kockemann@oru.se>
2023-01-24 15:59:49 +0100

    Fixed import

--- external/ros1/aiddl_external_ros1/aiddl_external_ros1/topic/sender_server.py
+++ external/ros1/aiddl_external_ros1/aiddl_external_ros1/topic/sender_server.py
@@ -5,6 +5,7 @@ import grpc
 from aiddl_core.parser import parser
 from aiddl_external_grpc_python.sender import SenderServer
 from aiddl_external_grpc_python.converter import Converter
+import aiddl_external_grpc_python.generated.empty_pb2 as empty_pb2
 
 class TopicSenderServer(SenderServer):
     def __init__(self, port, publisher, f_convert, verbose=False):
@@ -20,4 +21,4 @@ class TopicSenderServer(SenderServer):
         if self.verbose:
             print("Converted to:", ros_msg)
         self.pub.publish(ros_msg)
-        return sender_pb2.Empty()
+        return empty_pb2.Empty()

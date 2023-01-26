0e3f9876b3b49f7b1c50b95c071a8b1fddd7fd94
Uwe Köckemann <uwe.kockemann@oru.se>
2023-01-26 16:50:54 +0100

    Removing test

--- external/ros1/aiddl_external_ros1/aiddl_external_ros1/topic/receiver_server.py
+++ external/ros1/aiddl_external_ros1/aiddl_external_ros1/topic/receiver_server.py
@@ -27,7 +27,6 @@ class TopicReceiverServer(ReceiverServer):
         while self.queue_lock:
             pass
         self.queue_lock = True
-        print("RECEIVED DATA", data)
         self.message_queue.append(data)
         self.queue_lock = False
 

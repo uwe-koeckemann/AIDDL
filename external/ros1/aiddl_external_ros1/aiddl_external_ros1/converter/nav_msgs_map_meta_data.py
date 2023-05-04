0a318ff76e7c8b0236404514b667447226911779
Uwe Köckemann <uwe.kockemann@oru.se>
2023-05-04 09:37:42 +0200

    Fix

--- external/ros1/aiddl_external_ros1/aiddl_external_ros1/converter/nav_msgs_map_meta_data.py
+++ external/ros1/aiddl_external_ros1/aiddl_external_ros1/converter/nav_msgs_map_meta_data.py
@@ -3,12 +3,12 @@ from aiddl_core.representation import List
 from aiddl_core.representation import KeyVal
 
 from aiddl_external_ros1.converter.constant import RESOLUTION, WIDTH, HEIGHT, ORIGIN
-from aiddl_external_ros1.converter.geometry_msgs_pose import pose_2_aiddl
+from aiddl_external_ros1.converter.geometry_msgs_pose import PoseConverter
 
 def map_meta_data_2_aiddl(map_msg):
     return List(
         KeyVal(RESOLUTION, Real(map_msg.resolution)),
         KeyVal(WIDTH, Real(map_msg.width)),
         KeyVal(HEIGHT, Real(map_msg.height)),
-        KeyVal(ORIGIN, pose_2_aiddl(map_msg.origin))
+        KeyVal(ORIGIN, PoseConverter.pose_2_aiddl(map_msg.origin))
     )
